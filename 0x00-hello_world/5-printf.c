388e02f5ad46969c98edce72deebe1cc1bddd9e4
NDAMHOSEA <ndamhosea@gmail.com>
2023-03-16 13:38:29 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Print statement without put as a function
+ * Return: 0 (Ok)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
