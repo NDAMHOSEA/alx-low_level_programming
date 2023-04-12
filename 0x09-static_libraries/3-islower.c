baf237aa2329e88bbdcd0aa4b6ff61c565f32b39
NDAMHOSEA <ndamhosea@gmail.com>
2023-04-12 09:33:12 -0700

    0x09_C-static_libraries

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower -Cchecks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
